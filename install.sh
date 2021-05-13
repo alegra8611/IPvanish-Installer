403bf40d14759ecae3ff68c913c69c9b31b4eca5
alegra8611 <76691298+alegra8611@users.noreply.github.com>
2021-05-14 04:15:12 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,40 @@ export DIR=~/IPvanish
 export SCRIPT=~/IPvanish-Installer/
 export a=1
 export b=1
+export BWHITE='\033[1;37m'
+export BBLUE='\033[1;34m'
+export NC='\033[0m'
+clear
+echo -e ${BBLUE}"---------------------------------------------------------------------------------------------------------------------------------------------------------" 
+echo -e ${BWHITE}"              DMMMMMMD                                                                          DMMMMMMD  "                                                            
+echo -e ${BWHITE}"          MMMMMMMMMMMMMMMM                                                                   MMMMMMMMMMMMMMMM   "                                             
+echo -e ${BWHITE}"        MMMMMMMMMMMMMMMMMMMM                                                               MMMMMMMMMMMMMMMMMMMM      "       
+echo -e ${BWHITE}"       MMMMMMMMMMMMMMMMMMMMMM.                                                            MMMMMMMMMMMMMMMMMMMMMM.    "                       
+echo -e ${BWHITE}"      MMMMMMMMMMMMMMMMMMMMMMMMMM                                                        MMMMMMMMMMMMMMMMMMMMMMMMMM   "      
+echo -e ${BWHITE}"     MMMMMMMMMMMMMMMMMMMMMMMMMMMM                                                      MMMMMMMMMMMMMMMMMMMMMMMMMMMM  "      
+echo -e ${BWHITE}"    .MMMMMMMMMMMMMMMMMMMMMMMMMMMM                                                     .MMMMMMMMMMMMMMMMMMMMMMMMMMMM "
+echo -e ${BWHITE}"    MMMMMMMMMMMMMMMMMMMMMMMMMMMMMM                                                    MMMMMMMMMMMMMMMMMMMMMMMMMMMMMM  "     
+echo -e ${BWHITE}"    MMMMMMMMMMMMMMMMMMMMMMMMMMMMMM                                                    MMMMMMMMMMMMMMMMMMMMMMMMMMMMMM  "    
+echo -e ${BWHITE}"    MM+  MMMMMMMMMMMMMMMMMMMM$  MM                                                    MM+  MMMMMMMMMMMMMMMMMMMM$  MM   "
+echo -e ${BWHITE}"    +M.    ZMMMMMMMMMMMMMMO     M     ________     __  __                             +M.    ZMMMMMMMMMMMMMMO     M    "
+echo -e ${BWHITE}"     MM       MMMMMMMMMM       MM    /  _/ __ \   / / / /___  ____  ____  ___  _____   MM       MMMMMMMMMM       MM    "
+echo -e ${BWHITE}"    MMD         MMMM.        .MM     / // /_/ /  / /_/ / __ \/ __ \/ __ \/ _ \/ ___/  MMD         MMMM.        .MM     "
+echo -e ${BWHITE}"     MMMMMM    MMM=MMI   DMMMMM    _/ // ____/  / __  / /_/ / /_/ / /_/ /  __/ /       MMMMMM    MMM=MMI   DMMMMM       "
+echo -e ${BWHITE}"      MMMMMMMMMMM  +MMMMMMMMMMM   /___/_/      /_/ /_/\____/ .___/ .___/\___/_/         MMMMMMMMMMM  +MMMMMMMMMMM "
+echo -e ${BWHITE}"     MMMMMMMMMMM 8M MMMMMMMMMMMD                          /_/   /_/                    MMMMMMMMMMM 8M MMMMMMMMMMMD "
+echo -e ${BWHITE}"     MMMMM   MMM.MM MMMM    MMM                                                        MMMMM   MMM.MM MMMM    MMM "
+echo -e ${BWHITE}"      MMM7   MMMMMMMMMMM    M8           Author: Alegra_Punisher8611876                 MMM7   MMMMMMMMMMM    M8 "        
+echo -e ${BWHITE}"             MM MM M MMM                                                                       MM MM M MMM      "                                                               
+echo -e ${BWHITE}"             MM MM,M MM                                                                        MM MM,M MM      "         
+echo -e ${BWHITE}"             MM.MM=M MM                                                                        MM.MM=M MM		"
+echo -e ${BWHITE}"             MM~MM+M MM                                                                        MM~MM+M MM    "                                                               
+echo -e ${BWHITE}"             8M~MM+M MM                                                                        8M~MM+M MM   "            
+echo -e ${BWHITE}"             ,M~MMIM MM                                                                        ,M~MMIM MM  "              
+echo -e ${BWHITE}"              M:MMZM MM                                                                         M:MMZM MM  "                   
+echo -e ${BWHITE}"              M.MMIM MM                                                                         M.MMIM MM "
+echo -e ${BBLUE}"---------------------------------------------------------------------------------------------------------------------------------------------------------" 
+echo -e ${NC} ""
+echo ""
 
 sudo apt update
 sudo apt install openvpn
